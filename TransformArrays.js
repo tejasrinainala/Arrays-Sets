266cc2fef8991d31c2e2f8c0785a8ddcc2dbc419
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-10 11:45:11 +0530

    Create TransformArrays.js

---
+++ TransformArrays.js
@@ -0,0 +1,38 @@
+// Challenge: 
+// Use reduce() to sum the weights of all the cars
+// Stretch goal: 
+// Use reduce to sum the weights of only the electric cars 
+
+const cars = [
+  { name: "Toyota", isElectric: false, weight: 1320 },
+  { name: "Ford", isElectric: false, weight: 1400 },
+  { name: "Volkswagen", isElectric: false, weight: 1370 },
+  { name: "Honda", isElectric: false, weight: 1375 },
+  { name: "Tesla", isElectric: true, weight: 1750 },
+  { name: "BMW", isElectric: true, weight: 1350 },  
+];
+
+
+//solution
+const totalWeight1 = cars.reduce((accumulator, car) => {
+        return accumulator + car.weight;
+}, 0)
+console.log(totalWeight1)
+
+
+//stretch goal
+const totalWeight2 = cars.reduce((accumulator, car) => {
+    if (car.isElectric) {
+        return accumulator + car.weight;
+    } else {
+        return accumulator;
+    }
+}, 0)
+
+console.log(totalWeight2);
+
+
+
+//output
+8565
+3100
